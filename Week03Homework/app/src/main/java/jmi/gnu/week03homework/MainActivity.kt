d575be0aa645588f7ba3450092d241a45d6abcd4
jung0115 <jungim7490@gmail.com>
2024-03-24 23:19:17 +0900

    [WEEK03] #1 - 글자 나타내기(Toast), 홈페이지 열기 event

--- Week03Homework/app/src/main/java/jmi/gnu/week03homework/MainActivity.kt
+++ Week03Homework/app/src/main/java/jmi/gnu/week03homework/MainActivity.kt
@@ -1,7 +1,10 @@
 package jmi.gnu.week03homework
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import jmi.gnu.week03homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         // ViewBinding
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 글자 나타나기
+        binding.btnViewText.setOnClickListener {
+            Toast.makeText(applicationContext, binding.edittext.text,
+                Toast.LENGTH_SHORT).show()
+        }
+
+        // 홈페이지 열기
+        binding.btnOpenPage.setOnClickListener {
+            // url을 브라우저 앱에서 실행
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(binding.edittext.text.toString()))
+            startActivity(intent)
+        }
     }
 
     override fun onDestroy() {
