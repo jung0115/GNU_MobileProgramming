ed4d5a8745f3a8f6a2cb78dafd23ba5a6dfd4620
jung0115 <jungim7490@gmail.com>
2024-03-24 22:38:33 +0900

    [WEEK03] #1 - ViewBinding 설정, status bar 색상 변경

--- Week03Homework/app/src/main/java/jmi/gnu/week03homework/MainActivity.kt
+++ Week03Homework/app/src/main/java/jmi/gnu/week03homework/MainActivity.kt
@@ -2,10 +2,22 @@ package jmi.gnu.week03homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jmi.gnu.week03homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ViewBinding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
