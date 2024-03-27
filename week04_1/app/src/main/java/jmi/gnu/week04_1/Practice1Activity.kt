450daeccd5a7186bccbdcc9d725390f24145d50b
jung0115 <jungim7490@gmail.com>
2024-03-27 13:55:58 +0900

    [WEEK04] #4 - 실습 4-2 코드 Practice1Activity로 이동

---
+++ week04_1/app/src/main/java/jmi/gnu/week04_1/Practice1Activity.kt
@@ -0,0 +1,11 @@
+package jmi.gnu.week04_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Practice1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice1)
+    }
+}
