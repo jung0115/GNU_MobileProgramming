8a12e1a8ce102c674bd9f9cbeea8a11a64294b0f
jung0115 <jungim7490@gmail.com>
2024-05-03 18:26:41 +0900

    check

--- MiddleTest/app/src/main/java/jmi/gnu/middletest/MainActivity.kt
+++ MiddleTest/app/src/main/java/jmi/gnu/middletest/MainActivity.kt
@@ -2,10 +2,33 @@ package jmi.gnu.middletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.edit_text)
+
+        // 에디트 텍스트 이벤트
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val text = editText.text
+                Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 }
