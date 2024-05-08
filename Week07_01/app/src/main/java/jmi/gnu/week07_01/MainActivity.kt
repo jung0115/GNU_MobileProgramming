5d876e431c3b9e13641c3459fd8640759b203f23
jung0115 <jungim7490@gmail.com>
2024-05-08 13:30:08 +0900

    [Week07] #17 - 바탕색 변경 앱 만들기(Menu)

--- Week07_01/app/src/main/java/jmi/gnu/week07_01/MainActivity.kt
+++ Week07_01/app/src/main/java/jmi/gnu/week07_01/MainActivity.kt
@@ -1,11 +1,64 @@
 package jmi.gnu.week07_01
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import jmi.gnu.week07_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ViewBinding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        var mInflater = menuInflater
+        mInflater.inflate(R.menu.menu1, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.itemRed -> {
+                binding.baseLayout.setBackgroundColor(Color.RED)
+                return true
+            }
+            R.id.itemGreen -> {
+                binding.baseLayout.setBackgroundColor(Color.GREEN)
+                return true
+            }
+            R.id.itemBlue -> {
+                binding.baseLayout.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            R.id.subRotate -> {
+                binding.button1.rotation = 45f
+                return true
+            }
+            R.id.subSize -> {
+                binding.button1.scaleX = 2f
+                return true
+            }
+        }
+
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
