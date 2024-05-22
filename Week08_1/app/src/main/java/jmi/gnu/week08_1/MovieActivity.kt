2bed1df4b4816c9f4224e21f43b98f2a752e9e78
jung0115 <jungim7490@gmail.com>
2024-05-22 16:29:59 +0900

    [Week08] #22 - 실습 11-2 : 영화 포스터 보기2

--- Week08_1/app/src/main/java/jmi/gnu/week08_1/MovieActivity.kt
+++ Week08_1/app/src/main/java/jmi/gnu/week08_1/MovieActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Gallery
+import android.widget.ImageView
 import jmi.gnu.week08_1.databinding.ActivityMovieBinding
 
 class MovieActivity : AppCompatActivity() {
@@ -18,11 +22,47 @@ class MovieActivity : AppCompatActivity() {
         _binding = ActivityMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var galleryAdapter = MyGalleryAdpater(this)
+        binding.gallery.adapter = galleryAdapter
 
     }
 
     inner class MyGalleryAdpater(var context: Context) : BaseAdapter() {
-        var posterId = arrayOf(R.drawable.img_poster1)
+        var posterId = arrayOf(R.drawable.img_poster1, R.drawable.img_poster2, R.drawable.img_poster3, R.drawable.img_poster4, R.drawable.img_poster5,
+            R.drawable.img_poster6, R.drawable.img_poster7, R.drawable.img_poster8, R.drawable.img_poster9, R.drawable.img_poster10)
+
+        override fun getCount(): Int {
+            return posterId.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return posterId[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var imageView: ImageView = if (convertView == null) {
+                ImageView(context).apply {
+                    layoutParams = Gallery.LayoutParams(200, 300)
+                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    setPadding(5, 5, 5, 5)
+                }
+            } else {
+                convertView as ImageView
+            }
+            imageView.setImageResource(posterId[position])
+
+            imageView.setOnClickListener {
+                binding.imagePoster.scaleType = ImageView.ScaleType.FIT_CENTER
+                binding.imagePoster.setImageResource(posterId[position])
+                false
+            }
+
+            return imageView
+        }
     }
 
 
